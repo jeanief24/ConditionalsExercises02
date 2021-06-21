6348cab36ec65f29a110ac5f1c1e105f11289331
JeanieFNJ <jeanie@outlook.com>
2021-06-21 14:39:33 +0000

    Testing === and ==

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let shuttleSpeed = 15000;
 
 // 3) Write conditional expressions to satisfy the following safety rules:
 
+if (engineIndicatorLight == "green") {
+   console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+   console.log("engines are preparing to start");
+} else {
+   console.log("engines are off");
+}
+
 // a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
 if (crewStatus) {
   console.log ("Crew Ready")
